ab6d8b866d7f0144c1e30984657a261bba59ed4a
JhoannS <sebastianzamudio2004@gmail.com>
2024-11-08 13:10:22 -0500

    creacion de header, banner

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
+// tailwind.config.js
+export const content = [
+	'./src/**/*.{html,js,astro,jsx,tsx}',
+];
+export const theme = {
+	extend: {
+		colors: {
+			azul_claro: '#87A2FF',
+			azul_rey: '#0B2F9F',
+			azul_oscuro: '#0F1035',
+			negro: '#070F2B',
+			blanco: '#F5F5F5',
+		},
 	},
-	plugins: [],
-}
+};
+export const plugins = [];
+  
