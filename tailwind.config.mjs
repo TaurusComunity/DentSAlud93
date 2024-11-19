8ac87d4339cfe79cf2a46f677f509fa0d68dfa55
JhoannS <sebastianzamudio2004@gmail.com>
2024-11-18 23:50:22 -0500

    Remasterizacion de dentsalud93

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,14 +6,25 @@ module.exports = {
 	theme: {
 	  extend: {
 		colors: {
-		  azul_claro: '#87A2FF', 
-		  azul_rey: '#0B2F9F',
-		  azul_oscuro: '#0F1035',
+		  primario: '#5679F0', 
+		  secundario: '#0B2F9F',
+		  secundario2: '#0F1035',
 		  negro: '#070F2B',
 		  blanco: '#F5F5F5',
 		},
+		animation: {
+			'slide-down': 'slideDown 0.3s ease-out forwards',
+		  },
+		  keyframes: {
+			slideDown: {
+			  '0%': { opacity: 0, transform: 'translateY(-10px)' },
+			  '100%': { opacity: 1, transform: 'translateY(0)' },
+			},
 	  },
 	},
-	plugins: [],
-  };
+	plugins: [
+	
+	],
+  }
+}
   
